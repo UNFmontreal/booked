8b5175bc7eefb0ffd2a23920ab9d5b0015067c99
Basile <basile.pinsard@gmail.com>
2019-05-22 17:23:52 -0400

    Update en_gb.php

--- lang/en_gb.php
+++ lang/en_gb.php
@@ -28,33 +28,4 @@ class en_gb extends en_us
         parent::__construct();
     }
 
-    protected function _LoadDates()
-    {
-        $dates = parent::_LoadDates();
-
-        // change defaults here
-        $dates['general_date'] = 'd/m/Y';
-        $dates['general_datetime'] = 'd/m/Y H:i:s';
-        $dates['schedule_daily'] = 'l, d/m/Y';
-        $dates['reservation_email'] = 'd/m/Y @ H:i (e)';
-        $dates['res_popup'] = 'd/m/Y H:i';
-        $dates['dashboard'] = 'l, d/m/Y H:i';
-        $dates['period_time'] = "H:i";
-        $dates['timepicker'] = 'H:i';
-        $dates['general_date_js'] = "dd/mm/yy";
-		$dates['short_datetime'] = 'j/n/y H:i';
-		$dates['schedule_daily'] = 'l, d/m/Y';
-		$dates['res_popup_time'] = 'D, d/n H:i';
-		$dates['short_reservation_date'] = 'j/n/y H:i';
-		$dates['mobile_reservation_date'] = 'j/n H:i';
-        $dates['general_time_js'] = 'H:mm';
-        $dates['timepicker_js'] = 'H:i';
-        $dates['momentjs_datetime'] = 'D/M/YY H:mm';
-		$dates['calendar_time'] = 'H:mm';
-		$dates['calendar_dates'] = 'd M';
-        $dates['report_date'] = '%d/%m';
-
-        $this->Dates = $dates;
-        return $this->Dates;
-    }
 }
